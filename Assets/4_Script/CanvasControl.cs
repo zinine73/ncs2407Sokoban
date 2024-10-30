98df159ed0b798d3821a3257b07a77f14d2c1f08
zinine73 <zinine@naver.com>
2024-10-30 15:48:11 +0900

    game

--- Assets/4_Script/CanvasControl.cs
+++ Assets/4_Script/CanvasControl.cs
@@ -5,14 +5,18 @@ using UnityEngine.UI;
 
 public class CanvasControl : MonoBehaviour
 {
+    [SerializeField] private Button btnAudio; // 효과음 켜고 끄기 버튼
     [SerializeField] private Button btnRestart; // 다시하기 버튼
     [SerializeField] private Button btnUndo; // 되돌리기 버튼
     [SerializeField] private GameObject panelTitle; // 타이틀 화면
     [SerializeField] private GameObject panelChooseMap; // 맵 고르기 화면
     [SerializeField] private GameObject panelPlay; // 플레이 화면
     [SerializeField] private GameObject panelGoal; // 클리어 시 화면
+    [SerializeField] private GameObject panelController; // 안드로이드 시 입력 UI 패널
     [SerializeField] private MapButton mapButtonPrefab; // 맵버튼 프레팝 연결
     [SerializeField] private RectTransform contentTransform; // 스크롤뷰에서 버튼 프레팝이 들어갈 위치
+    [SerializeField] private Slider sliderZoom; // 화면 줌 슬라이더
+    [SerializeField] private Sprite[] audioOnOff; // 효과음 켜고 끄기 스프라이트 배열
     private MapButton[] mapButton = null; // 맵버튼 배열
 
     // 버튼이 미리 만들어져 있어야 클리어여부가 제대로 표시된다. 그래서 Start가 아닌 Awake
@@ -27,6 +31,35 @@ public class CanvasControl : MonoBehaviour
             // 번호는 1번부터 시작하게
             mapButton[i].SetNumber(i + 1);
         }
+
+        // 화면줌 슬라이더의 초기값 설정
+        sliderZoom.minValue = GameManager.MAX_ZOOM;
+        sliderZoom.maxValue = GameManager.MIN_ZOOM;
+        sliderZoom.value = GameManager.START_ZOOM;
+
+        AudioState audio = (AudioState)PlayerPrefs.GetInt(GameManager.KEY_AUDIO, (int)AudioState.Enable);
+        btnAudio.GetComponent<Image>().sprite = audioOnOff[(int)audio];
+    }
+
+    /// <summary>
+    /// 슬라이더로 줌을 조정한다
+    /// </summary>
+    public void SetSliderZoom()
+    {
+        GameManager.instance.TargetZoom = sliderZoom.value;
+    }
+
+    /// <summary>
+    /// 효과음 버튼 눌러서 켜고 끄기 상태 변환
+    /// </summary>
+    public void SetAuidoOnOff()
+    {
+        AudioState value = GameManager.instance.AudioEnable;
+        if (value == AudioState.Disable) value = AudioState.Enable;
+        else value = AudioState.Disable;
+        GameManager.instance.AudioEnable = value;
+        PlayerPrefs.SetInt(GameManager.KEY_AUDIO, (int)value);
+        btnAudio.GetComponent<Image>().sprite = audioOnOff[(int)value];
     }
 
     /// <summary>
@@ -53,6 +86,7 @@ public class CanvasControl : MonoBehaviour
             panelChooseMap.SetActive(false);
             panelPlay.SetActive(false);
             panelGoal.SetActive(false);
+            panelController.SetActive(false);
         }
         else if (state == GameState.ChooseMap)
         {
@@ -60,9 +94,22 @@ public class CanvasControl : MonoBehaviour
             panelChooseMap.SetActive(true);
             panelPlay.SetActive(false);
             panelGoal.SetActive(false);
+            panelController.SetActive(false);
+
+            string str = PlayerPrefs.GetString(GameManager.KEY_PP);
             for (int i = 0; i < GameManager.MAX_MAP_COUNT; i++)
             {
-                // todo 맵 클리어 상황에 따라 별 표시
+                if (str[i].Equals('X'))
+                {
+                    mapButton[i].GetComponent<Button>().interactable = false;
+                    mapButton[i].SetCleared(false);
+                }
+                else
+                {
+                    mapButton[i].GetComponent<Button>().interactable = true;
+                    // 맵 클리어 상황에 따라 별 표시
+                    mapButton[i].SetCleared(str[i].Equals('C'));
+                }
             }
         }
         else if (state == GameState.Goal)
@@ -75,6 +122,9 @@ public class CanvasControl : MonoBehaviour
             panelChooseMap.SetActive(false);
             panelPlay.SetActive(true);
             panelGoal.SetActive(false);
+#if UNITY_ANDROID && !UNITY_EDITOR
+            panelController.SetActive(true);
+#endif            
             btnRestart.interactable = false;
             btnUndo.interactable = false;
         }
--- Assets/4_Script/GameManager.cs
+++ Assets/4_Script/GameManager.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using System.Linq;
+using System.Text;
+using System;
+using System.IO;
+using UnityEngine.Networking;
 
 [System.Serializable]
 public struct MapStruct // 맵구조체
@@ -22,10 +26,22 @@ public enum GameState // 게임 상태
     Title, ChooseMap, Play, Goal
 }
 
+public enum AudioState // 효과음 상태
+{
+    Disable, Enable
+}
+
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null; // 싱글톤
+    public static string KEY_PP = "OPEN_MAP"; // 맵 오픈 정보를 저장할 키
+    public static string KEY_AUDIO = "AUDIO"; // 효과음 상태 저장할 키
     public static int MAX_MAP_COUNT = 96; // 전체 맵 갯수
+    public static float MAX_ZOOM = 4f; // 최대 줌인 크기
+    public static float MIN_ZOOM = 11f; // 최소 줌아웃 크기
+    public static float START_ZOOM = 5f; // 시작 줌 크기
+    private const float SENSITIVITY = 1f; // 줌 인아웃 감도
+    private const float ZOOM_SPEED = 30f; // 줌 카메라 속도
     private const float MOVE_SPEED = 7.0f; // 플레이어 움직이는 속도
     [SerializeField] private CanvasControl canvas; // 캔버스
     [SerializeField] private GameObject mapPrefab; // 맵 오브젝트 프레팝
@@ -42,16 +58,57 @@ public class GameManager : MonoBehaviour
     private Vector2 copyPosition; // 재실행에 사용되는 플레이어 위치
     private Vector2 playerPosition; // 플레이어 위치
     private Vector2 playerDirection; // 플레이어 방향
+    private Vector2 undoPosition; // 되돌리기 할 위치
+    private int? undoBoxIndex; // 되돌리기 할 상자 번호(없을수도 있으니 null 값을 가질 수 있게)
+    private bool isRunGoalCoroutine; // 코루틴 하나만 돌게 체크하는 변수
+    private float targetZoom; // 줌인아웃 크기 변수
+    private Camera mainCamera; // 카메라
+    private AudioSource audioSource; // 효과음 소스
+    private AudioState audioEnable; // 효과음 상태를 저장할 변수
+
+    [Header("[ AUDIO ]")]
+    public AudioClip audioMenu;
+    public AudioClip audioWalk;
+    public AudioClip audioBox;
+    public AudioClip audioGoal;
+
+    [Header("[ JSON ]")]
     public int mapNumber; // 맵 번호
     public MapStruct map; // 맵 정보
+
+    // 줌 크기 변수 프로퍼티
+    public float TargetZoom
+    {
+        get => targetZoom;
+        set => targetZoom = Mathf.Clamp(value, MAX_ZOOM, MIN_ZOOM);
+    }
+    // 효과음 상태 프로퍼티
+    public AudioState AudioEnable
+    {
+        get => audioEnable;
+        set => audioEnable = value;
+    }
 
     private void Awake()
     {
         if (instance == null) instance = this;
+
+        // 카메라는 메인카메라로 지정
+        mainCamera = Camera.main;
+        // 줌 방식은 ortho~, 시작 크기 지정
+        mainCamera.orthographicSize = TargetZoom = START_ZOOM;
+
+        audioSource = GetComponent<AudioSource>();
+        audioEnable = (AudioState)PlayerPrefs.GetInt(KEY_AUDIO, (int)AudioState.Enable);
     }
 
     private void Start()
     {
+        // KEY_PP 값이 없거나 이전 값이면 값을 만들어 저장한다
+        if (PlayerPrefs.GetString(KEY_PP).Length < MAX_MAP_COUNT)
+        {
+            MakeOpenClearMapData(false);
+        }
         // 시작 맵 번호는 1
         mapNumber = 1;
         // 로직에 사용될 상자와 골의 리스트 만들기
@@ -60,6 +117,15 @@ public class GameManager : MonoBehaviour
 
         ClearAllObject();
         ChangeGameState(GameState.Title);
+    }
+
+    // clip 효과음 재생
+    private void PlayAudio(AudioClip clip)
+    {
+        if (audioEnable == AudioState.Enable)
+        {
+            audioSource.PlayOneShot(clip);
+        }
     }
 
     // 자식 오브젝트 지우기
@@ -81,6 +147,11 @@ public class GameManager : MonoBehaviour
         DeleteChild(boxes);
         // 맵고르기 화면에서는 플레이어 안 보이게
         player.SetActive(false);
+        // 되돌리기 상자는 선택된게 없으니 null
+        undoBoxIndex = null;
+        // 코루틴이 돌고 있다면 모두 멈춤
+        StopAllCoroutines();
+        isRunGoalCoroutine = false;
     }
 
     // 게임 상태 바꾸기
@@ -96,6 +167,18 @@ public class GameManager : MonoBehaviour
         return new Vector2(x - map.SizeX * 0.5f, y - map.SizeY * 0.5f);
     }
 
+    /// <summary>
+    /// 안드로이드일때 UI 버튼 입력
+    /// </summary>
+    /// <param name="dir">0:위, 1: 아래, 2:왼쪽, 3:오른쪽</param>
+    public void OnUIKeyInput(int dir)
+    {
+        if (dir == 0)       MoveProcess(0, 1);
+        else if (dir == 1)  MoveProcess(0, -1);
+        else if (dir == 2)  MoveProcess(-1, 0);
+        else if (dir == 3)  MoveProcess(1, 0);
+    }
+
     private void CheckKeyInput()
     {
         // 키가 눌리면
@@ -104,60 +187,89 @@ public class GameManager : MonoBehaviour
             // x,y 축 값을 -1,0,1 로만 받아서
             float moveX = Input.GetAxisRaw("Horizontal");
             float moveY = Input.GetAxisRaw("Vertical");
-            // 대각선 입력은 무시하고
-            if (moveX == 0 || moveY == 0)
-            {
-                // 방향값을 정한다
-                playerDirection = new Vector2(moveX, moveY);
-                // 방향값에 따른 플레이어 애니메이션 실행
-                player.GetComponent<PlayerAnimation>().SetAnimation(playerDirection);
+            MoveProcess(moveX, moveY);
+        }
+    }
 
-                // 움직일 위치를 변수로 정하고
-                Vector2 tryPlayerPos = playerPosition + playerDirection; 
-                // 바닥이면 (board엔 grpund 아니면 wall만 남아있다)
-                if (board[(int)tryPlayerPos.x, (int)tryPlayerPos.y] == MapEnum.Ground)
+    private void MoveProcess(float moveX, float moveY)
+    {
+        // 클리어한 상황이면 키 입력 무시
+        if (isRunGoalCoroutine) return;
+
+        // 대각선 입력은 무시하고
+        if (moveX == 0 || moveY == 0)
+        {
+            // 방향값을 정한다
+            playerDirection = new Vector2(moveX, moveY);
+            // 방향값에 따른 플레이어 애니메이션 실행
+            player.GetComponent<PlayerAnimation>().SetAnimation(playerDirection);
+        }
+
+        // 움직일 위치를 변수로 정하고
+        Vector2 tryPlayerPos = playerPosition + playerDirection;
+        // 바닥이면 (board엔 grpund 아니면 wall만 남아있다)
+        if (board[(int)tryPlayerPos.x, (int)tryPlayerPos.y] == MapEnum.Ground)
+        {
+            // 움직임이 발생했으므로 버튼 다시하기, 되돌리기 버튼 활성화
+            canvas.SetRestartButton(true);
+            canvas.SetUndoButton(true);
+            // 움직일 위치에 상자가 있다면
+            if (boxList.Contains(tryPlayerPos))
+            {
+                // 상자가 움직일 위치도 변수로 정하고
+                Vector2 tryBoxPos = tryPlayerPos + playerDirection;
+                // 바닥이면 상자가 움직일 수 있다
+                if (board[(int)tryBoxPos.x, (int)tryBoxPos.y] == MapEnum.Ground)
                 {
-                    // 움직임이 발생했으므로 버튼 다시하기, 되돌리기 버튼 활성화
-                    canvas.SetRestartButton(true);
-                    canvas.SetUndoButton(true);
-                    // 움직일 위치에 상자가 있다면
-                    if (boxList.Contains(tryPlayerPos))
+                    // 움직일 위치에 상자가 없어야 움직일 수 있다
+                    if (boxList.Contains(tryBoxPos) == false)
                     {
-                        // 상자가 움직일 위치도 변수로 정하고
-                        Vector2 tryBoxPos = tryPlayerPos + playerDirection;
-                        // 바닥이면 상자가 움직일 수 있다
-                        if (board[(int)tryBoxPos.x, (int)tryBoxPos.y] == MapEnum.Ground)
+                        // 움직여야 할 상자를 상자리스트 중에서 찾는다
+                        for (int i = 0; i < boxList.Count; i++)
                         {
-                            // 움직일 위치에 상자가 없어야 움직일 수 있다
-                            if (boxList.Contains(tryBoxPos) == false)
+                            if ((tryPlayerPos.x == boxList[i].x) && (tryPlayerPos.y == boxList[i].y))
                             {
-                                // 움직여야 할 상자를 상자리스트 중에서 찾는다
-                                for (int i = 0; i< boxList.Count; i++)
-                                {
-                                    if ((tryPlayerPos.x == boxList[i].x) && (tryPlayerPos.y == boxList[i].y))
-                                    {
-                                        // 상자 이동
-                                        boxList[i] += playerDirection;
-                                        // 플레이어 이동
-                                        playerPosition = tryPlayerPos;
-                                        break;
-                                    }
-                                }
+                                // 되돌리기 위치에 플레이어 위치를 넣고
+                                undoPosition = playerPosition;
+                                // 되돌리기 상자 번호는 현재 상자 인덱스를 넣는다
+                                undoBoxIndex = i;
+                                // 상자 이동
+                                boxList[i] += playerDirection;
+                                // 플레이어 이동
+                                playerPosition = tryPlayerPos;
+                                // 상자 효과음 재생
+                                PlayAudio(audioBox);
+                                break;
                             }
                         }
                     }
-                    else // 움직일 위치에 상자가 없으므로
-                    {
-                        // 플레이어 이동
-                        playerPosition = tryPlayerPos;
-                    }
                 }
-            }   
+            }
+            else // 움직일 위치에 상자가 없으므로
+            {
+                // 되돌리기 위치에 플레이어 위치를 넣고
+                undoPosition = playerPosition;
+                // 되돌리기 상자는 없으니 null
+                undoBoxIndex = null;
+                // 플레이어 이동
+                playerPosition = tryPlayerPos;
+                // 이동 효과음 재생
+                PlayAudio(audioWalk);
+            }
         }
     }
 
     private void Update()
     {
+        // 카메라 줌인아웃으로 맵 전체를 한화면에 볼 수 있다
+        // 클리어 이후에도 줌아웃은 되게 한다
+#if UNITY_EDITOR || UNITY_STANDALONE // 안드로이드 실기기에서는 화면상의 슬라이더를 이용한다
+        // 프로퍼티를 이용해 min, max 값 사이로 조정한다
+        TargetZoom -= Input.mouseScrollDelta.y * SENSITIVITY;
+#endif
+        mainCamera.orthographicSize = Mathf.MoveTowards(mainCamera.orthographicSize,
+            targetZoom, ZOOM_SPEED * Time.deltaTime);
+
         // 플레이 상태에서만 아래 로직을 타면 된다
         if (state != GameState.Play) return;
 
@@ -185,12 +297,33 @@ public class GameManager : MonoBehaviour
             child.GetComponent<StuffChange>().IsOnGoal = check;
         }
         // 클리어했다면
-        if (isAllBoxGoal == true)
+        if ((isRunGoalCoroutine == false) && (isAllBoxGoal == true))
         {
-            // todo end goal
+            // 클리어 안내 코루틴 실행
+            StartCoroutine(Congratulations());
         }
     }
     
+    private IEnumerator Congratulations()
+    {
+        // 코루틴 시작
+        isRunGoalCoroutine = true;
+        // 클리어 정보 현재맵 위치는 'C'로 바꾼다
+        SetOpenClearMapData(mapNumber, "C");
+        // 다음 맵은 이미 오픈되어 있거나 클리어 된 상태면 아무것도 하지 않는다
+        SetOpenClearMapData(mapNumber + 1, "O", 'X');
+        // 맵선택 화면에서도 클리어 했다 표시, 다시하기 되돌리기 버튼은 비활성화
+        canvas.SetMapCleared(mapNumber);
+        // 1초 기다렸다가 (상자가 움직일 시간이 필요)
+        yield return new WaitForSeconds(1f);
+        // 정면 바라보는 애니메이션을 한번 실행해준다
+        player.GetComponent<PlayerAnimation>().SetAnimation(Vector2.zero);
+        // 클리어 효과음 재생
+        PlayAudio(audioGoal);
+        // 게임 상태를 클리어 상태로 바꾼다
+        ChangeGameState(GameState.Goal);
+    }
+
     // JSON 데이터로부터 맵 데이터 만들기
     private void MakeMapDataFromJSON()
     {
@@ -282,9 +415,16 @@ public class GameManager : MonoBehaviour
     /// <param name="number">맵 번호</param>
     public void StartPlay(int number)
     {
+        // 클릭 효과음 재생
+        PlayAudio(audioMenu);
+
         mapNumber = number;
-        MakeMapDataFromJSON();
-        InitializeMap();
+        // 맵 정보 불러오기 성공하면
+        if (LoadMapFromJSON())
+        {
+            MakeMapDataFromJSON();
+            InitializeMap();
+        }
     }
 
     /// <summary>
@@ -292,6 +432,8 @@ public class GameManager : MonoBehaviour
     /// </summary>
     public void Restart()
     {
+        // 클릭 효과음 재생
+        PlayAudio(audioMenu);
         // 정면 바라보게 하고
         player.GetComponent<PlayerAnimation>().SetAnimation(Vector2.zero);
         // 상자리스트에 카피리스트 복사해서 원래 상자위치 복원(Linq)
@@ -308,6 +450,8 @@ public class GameManager : MonoBehaviour
     /// </summary>
     public void ChooseMap()
     {
+        // 클릭 효과음 재생
+        PlayAudio(audioMenu);
         ClearAllObject();
         ChangeGameState(GameState.ChooseMap);
     }
@@ -317,6 +461,8 @@ public class GameManager : MonoBehaviour
     /// </summary>
     public void Exit()
     {
+        // 클릭 효과음 재생
+        PlayAudio(audioMenu);
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
@@ -329,6 +475,123 @@ public class GameManager : MonoBehaviour
     /// </summary>
     public void Undo()
     {
+        // 클릭 효과음 재생
+        PlayAudio(audioMenu);
+        // 되돌리기 상자가 선택된게 있다면
+        if (undoBoxIndex != null)
+        {
+            // 되돌릴 상자의 위치는 현재 플레이어의 위치(플레이어가 상자를 밀어서 그 자리에 갔으므로)
+            boxList[(int)undoBoxIndex] = playerPosition;
+        }
+        // 플레이어의 위치는 되돌리기 위치에 저장해 놓은 위치로
+        playerPosition = undoPosition;
+        // 되돌리기 버튼은 한번 사용했으므로 안 눌리게
         canvas.SetUndoButton(false);
+    }
+
+    private void MakeOpenClearMapData(bool isAllOpen)
+    {
+        var sb = new StringBuilder();
+        sb.Append("O");
+        for (int i = 1; i < MAX_MAP_COUNT; i++)
+        {
+            sb.Append(isAllOpen ? "O" : "X");
+        }
+        string str = sb.ToString();
+        PlayerPrefs.SetString(KEY_PP, str);
+    }
+
+    public void SetOpenClearMapData(int number, string value, char? compare = null)
+    {
+        string str = PlayerPrefs.GetString(KEY_PP);
+        int index = number - 1;
+        if (compare == null || str[index].Equals(compare))
+        {
+            str = str.Remove(index, 1);
+            str = str.Insert(index, value);
+            PlayerPrefs.SetString(KEY_PP, str);
+        }
+    }
+
+    // 오픈 정보 초기화
+    [ContextMenu("Sokoban/Reset Clear map save data", false, 1)]
+    private void ResetClearMapSave()
+    {
+        MakeOpenClearMapData(false);
+        Debug.Log("Reset clear map save data.");
+    }
+
+    // 치트 : 모든 맵 오픈
+    [ContextMenu("Sokoban/Cheat : Open all map (deleted clear info)", false, 2)]
+    private void CheatOpenAllMap()
+    {
+        MakeOpenClearMapData(true);
+        Debug.Log("Cheat Activate! All map opened.");
+    }
+
+    // 치트 : 맵 오픈
+    [ContextMenu("Sokoban/Cheat : Open one map (in GameManager)", false, 3)]
+    private void CheatOpenOneMap()
+    {
+        SetOpenClearMapData(mapNumber, "O");
+        Debug.Log($"Cheat Activate! map {mapNumber:00} opened.");
+    }
+
+    // 맵 데이터를 JSON 데이터로 만들기
+    [ContextMenu("Sokoban/Make JSON map data", false, 4)]
+    private void SaveMapToJSON()
+    {
+        // 파일 이름 정하고
+        string fileName = $"Maps/SokobanMap{mapNumber:00}.json";
+        // 저장 위치 정하고
+        string path = Path.Combine(Application.streamingAssetsPath, fileName);
+        // JSON 파일로 변환하여
+        string jsonData = JsonUtility.ToJson(map, true);
+        // 파일로 쓰기
+        File.WriteAllText(path, jsonData);
+        Debug.Log($"Make JSON data {mapNumber:00} is done.");
+    }
+
+    // JSON 데이터를 맵 데이터로 읽기
+    [ContextMenu("Sokoban/Load map from JSON", false, 5)]
+    private bool LoadMapFromJSON()
+    {
+        string fileName = $"Maps/SokobanMap{mapNumber:00}.json";
+        string path;
+#if UNITY_EDITOR || UNITY_STANDALONE
+        path = Path.Combine(Application.streamingAssetsPath, fileName);
+#elif UNITY_ANDROID
+        path = Path.Combine("jar:file://" + Application.dataPath + "!/assets/", fileName);
+#endif
+        try // 익셉션 나는 경우 대비
+        {
+            string jsonData;
+#if UNITY_ANDROID && !UNITY_EDITOR
+            if (path.StartsWith("jar:"))
+            {
+                UnityWebRequest www = UnityWebRequest.Get(path);
+                www.SendWebRequest();
+                while (!www.isDone) {}
+                jsonData = www.downloadHandler.text;
+            }
+            else
+            {
+                jsonData = File.ReadAllText(path);
+            }
+#else
+            // 파일에서 데이터를 읽고
+            jsonData = File.ReadAllText(path);
+#endif
+            // JSON에서 맵데이터로 변환
+            map = JsonUtility.FromJson<MapStruct>(jsonData);
+            Debug.Log($"Loaded JSON data {mapNumber:00} from file.");
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex);
+            Debug.Log($"Map : {mapNumber:00} data is null or broken...");
+            return false;
+        }
     }
 }
